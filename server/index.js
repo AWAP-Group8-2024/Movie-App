75372a60fbae387de903c0cf04fd003a36699c9e
fuzzykala <tangerinekey381@gmail.com>
2024-11-28 19:10:22 +0200

    fix: update static file serving path to correctly reference build directory

--- server/index.js
+++ server/index.js
@@ -3,7 +3,11 @@ import cors from "cors";
 import favoriteRouter from "./routers/FavoriteRouter.js";
 import userRouter from "./routers/UserRouter.js";
 import groupRouter from "./routers/GroupRouter.js";
+import { fileURLToPath } from "url";
 import path from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const port = process.env.PORT;
 
@@ -12,7 +16,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(path.join(__dirname, "../build")));
 
 // test message
 app.get("/api/message", (req, res) => {

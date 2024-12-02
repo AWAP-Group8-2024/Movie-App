c9dc3f1430394beb433a107de5552ebb8fdb4f51
fuzzykala <tangerinekey381@gmail.com>
2024-12-02 09:39:59 +0200

    refactor: remove unused tables and clean up database schema

--- server/db/db.sql
+++ server/db/db.sql
@@ -1,21 +1,4 @@
-
-DROP TABLE IF EXISTS content;
-DROP TABLE IF EXISTS theater;
 DROP TABLE IF EXISTS review;
-DROP TABLE IF EXISTS favorite;
-
-
--- For content
-CREATE TABLE content (
-    id SERIAL PRIMARY KEY,
-    title VARCHAR(100) NOT NULL,
-    type VARCHAR(20) CHECK (type IN ('movie', 'tv_series')),
-    release_date DATE
-);
-CREATE TABLE theater (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(50) UNIQUE NOT NULL
-);
 
 CREATE TABLE review (
     id SERIAL PRIMARY KEY,
@@ -28,19 +11,3 @@ CREATE TABLE review (
 );
 
 
-
--- Junction table
-CREATE TABLE group_account (
-    group_id INTEGER REFERENCES group(id) ON DELETE CASCADE,
-    account_id INTEGER REFERENCES account(id) ON DELETE CASCADE,
-    PRIMARY KEY (group_id, account_id)
-);
-CREATE TABLE favorite (
-    user_id INTEGER REFERENCES user(id) ON DELETE CASCADE,
-    content_id INTEGER REFERENCES content(id) ON DELETE CASCADE,
-    PRIMARY KEY (user_id, content_id)
-);
-
-
--- SELECT * FROM task
--- SELECT * FROM account

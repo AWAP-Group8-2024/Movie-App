12917b20adada446d1bbc1a133427feb3b031866
fuzzykala <tangerinekey381@gmail.com>
2024-12-02 11:01:15 +0200

    chore: update .gitignore to include package-lock.json and add react-rating-stars-component to package-lock.json refactor: modify review table schema to remove foreign key constraint on movie_id

--- server/db/db.sql
+++ server/db/db.sql
@@ -2,12 +2,10 @@ DROP TABLE IF EXISTS review;
 
 CREATE TABLE review (
     id SERIAL PRIMARY KEY,
-    movie_id INT REFERENCES movie(id) ON DELETE CASCADE,
+    movie_id INT,
     user_id INT REFERENCES account(id) ON DELETE CASCADE,
     description TEXT NOT NULL,
     rating INT CHECK (rating BETWEEN 0 AND 5),
     reviewer_email VARCHAR(50) NOT NULL,
     timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
-
-

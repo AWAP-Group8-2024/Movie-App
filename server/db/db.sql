e52ac9db34324422248959e9321be340665364e1
t3weja00 <156579740+t3weja00@users.noreply.github.com>
2024-12-02 11:02:54 +0200

    chnage the review table column data type to int to double to store the decimal values
    otherwise it wont insert the half of the star ratings

--- server/db/db.sql
+++ server/db/db.sql
@@ -19,10 +19,10 @@ CREATE TABLE theater (
 
 CREATE TABLE review (
     id SERIAL PRIMARY KEY,
-    movie_id INT REFERENCES movie(id) ON DELETE CASCADE,
+    movie_id INT,
     user_id INT REFERENCES account(id) ON DELETE CASCADE,
     description TEXT NOT NULL,
-    rating INT CHECK (rating BETWEEN 0 AND 5),
+    rating  double precision CHECK (rating BETWEEN 0 AND 5),
     reviewer_email VARCHAR(50) NOT NULL,
     timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
